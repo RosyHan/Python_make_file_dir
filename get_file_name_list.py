a8adc8aec6d6dc93e26b398fc169b04fb3cc9051
HanMengsi <745857498@qq.com>
2019-03-24 14:28:47 +0800

    Add files via upload

---
+++ get_file_name_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  test.py
+#  
+#  Copyright 2018 hanms <hanms@IAT-0102-0214>
+#  
+with open(r'E:\files\new.txt') as file_object:
+contents = file_object.read()
+print(contents)
